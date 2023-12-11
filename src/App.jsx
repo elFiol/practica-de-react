6474ac6490e86485f900cd7745f0d22ff142fa8d
Fiol Thiago <fiolthiago9@gmail.com>
2023-12-11 10:08:15 -0300

    agregamos libreria de boostrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
-  return (
-    <h1>hola mundo</h1>
-  )
+  return <h1 className=" display-3">hola mundo</h1>;
 }
 
-export default App
+export default App;
