f6fbfd93b0b72d74ea52d15dc8c064a35919dd95
Fiol Thiago <fiolthiago9@gmail.com>
2023-12-11 17:14:09 -0300

    agregamos props los botones del segundo componente que armamos: contador

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import SegundoTitulo from "./components/SegundoTitulo";
+import Contador from "./components/Contador";
 
 function App() {
   const anioActual = 2023;
@@ -9,6 +10,7 @@ function App() {
       {/* <SegundoTitulo></SegundoTitulo> */}
       {/* tambien se puede agregar con <></> */}
       <SegundoTitulo comision="c73i" anioActualProps={anioActual}></SegundoTitulo>
+      <Contador></Contador>
     </section>
   );
 }
---
+++ src/components/Contador.jsx
@@ -0,0 +1,11 @@
+const Contador = () => {
+    return (
+        <div className="text-center">
+            <h3>Contador</h3>
+            <p>0</p>
+            <button className="btn btn-primary">+1</button>
+        </div>
+    );
+};
+
+export default Contador;
