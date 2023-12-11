ac21e3b55968ff6a44e06b4295e3a01fd676ba46
Fiol Thiago <fiolthiago9@gmail.com>
2023-12-11 10:37:42 -0300

    agregamos el primer componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import SegundoTitulo from "./components/SegundoTitulo";
 
 function App() {
-  return <h1 className=" display-3">hola mundo</h1>;
+  return (
+    <section className="container my-4">
+      <h1 className=" display-3 text-center">contador con react</h1>
+      <SegundoTitulo></SegundoTitulo>
+      {/* tambien se puede agregar con <></> */}
+      <SegundoTitulo></SegundoTitulo>
+    </section>
+  );
 }
 
 export default App;
